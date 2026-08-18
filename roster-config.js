1ccd5b34ca16d90edf44cb9f78a1e3c804bf3c74
295ashfieldmassage-cyber <295ashfieldmassage@gmail.com>
2026-08-18 19:18:37 +1000

    Add files via upload

--- roster-config.js
+++ roster-config.js
@@ -24,12 +24,10 @@ window.WEEKLY_ROSTER = {
     ],
     wednesday: [
       { name: "BB", nationality: "Korean", height: "164 cm", weight: "51kg", age: 21, cup: "C", description: "Lovely Korean beauty", image: "images/BB.jpg", hoverImages:["images/BB1.jpg","images/BB2.jpg","images/BB3.jpg","images/BB4.jpg"] },
-      { name: "Jeniffer", nationality: "Singaporean", height: "168 cm", weight: "50 kg", age: 26, cup: "D", description: "Glamorous Singaporean team member", image: "images/Jenifer.jpg", hoverImages:["images/Jenifer1.jpg","images/Jenifer2.jpg","images/Jenifer3.jpg"] },
+      { name: "Lulu", nationality: "Chinese", height: "166cm", weight: "55kg", age: 26, cup: "D", description: "Excellent skills team member", image: "images/lulu.jpg", hoverImages:["images/lulu1.jpg","images/lulu2.jpg","images/lulu3.jpg"] },
       { name: "Mina", nationality: "Thai", height: "165 cm", weight: "57 kg", age: 28, cup: "E", description: "Friendly Thai team member", image: "images/mina.jpg", hoverImages:["images/Mina1.jpg","images/Mina2.jpg","images/Mina3.jpg"] },
       { name: "Enaya", nationality: "India", height: "164 cm", weight: "57 kg", age: 21, cup: "E", description: "Friendly India team member", image: "images/enaya.jpg", hoverImages:["images/enaya1.jpg"] },
-      { name: "Lucy", nationality: "Chinese", height: "162 cm", weight: "54 kg", age: 23, cup: "D", description: "Excellent Chinese massage practitioner", image: "images/lucy.jpg", hoverImages:["images/lucy1.jpg","images/lucy2.jpg","images/lucy3.jpg"] },
-      { name: "Lily", nationality: "Chinese", height: "168 cm", weight: "47kg", age: 21, cup: "B", description: "Fun playful Chinese team member", image: "images/lily.jpg", hoverImages:["images/lily1.jpg","images/lily2.jpg","images/lily3.jpg"] },
-      { name: "Sweet", nationality: "Philippines", height: "160 cm", weight: "47 kg", age: 24, cup: "C", description: "Friendly Philippines team member", image: "images/sweet.jpg", hoverImages:["images/sweet1.jpg","images/sweet2.jpg","images/sweet3.jpg"] }
+      { name: "Lucy", nationality: "Chinese", height: "162 cm", weight: "54 kg", age: 23, cup: "D", description: "Excellent Chinese massage practitioner", image: "images/lucy.jpg", hoverImages:["images/lucy1.jpg","images/lucy2.jpg","images/lucy3.jpg"] }
     ],
     thursday: [
       { name: "Isabella", nationality: "Singaporean", height: "168 cm", weight: "50 kg", age: 26, cup: "D", description: "Stunning Singaporean team member", image: "images/Isabella.jpg" },
@@ -42,15 +40,18 @@ window.WEEKLY_ROSTER = {
       { name: "Anna", nationality: "Australian", height: "166 cm", weight: "49 kg", age: 30, cup: "B", description: "Blonde, blue-eyed Australian team member", image: "images/anna.jpg" }
     ],
     friday: [
-      { name: "Rina", nationality: "Chinese", height: "169 cm", weight: "58 kg", age: 20, cup: "D", description: "University student", image: "images/Rina.jpg" },
-      { name: "Lucy", nationality: "Chinese", height: "162 cm", weight: "54 kg", age: 23, cup: "D", description: "Excellent Chinese massage practitioner", image: "images/lucy.jpg", hoverImages:["images/lucy1.jpg","images/lucy2.jpg","images/lucy3.jpg"] },
+      { name: "Sweet", nationality: "Philippines", height: "160 cm", weight: "47 kg", age: 24, cup: "C", description: "Friendly Philippines team member", image: "images/sweet.jpg", hoverImages:["images/sweet1.jpg","images/sweet2.jpg","images/sweet3.jpg"] },
+      { name: "Nora", nationality: "Chinese", height: "170 cm", weight: "47kg", age: 20, cup: "B", description: "Young cute Chinese team member", image: "images/nora.jpg", hoverImages:["images/nora1.jpg","images/nora2.jpg","images/nora3.jpg"] },
       { name: "Lily", nationality: "Chinese", height: "168 cm", weight: "47kg", age: 21, cup: "B", description: "Fun playful Chinese team member", image: "images/lily.jpg", hoverImages:["images/lily1.jpg","images/lily2.jpg","images/lily3.jpg"] },
       { name: "Isabella", nationality: "Singaporean", height: "168 cm", weight: "50 kg", age: 26, cup: "D", description: "Stunning Singaporean team member", image: "images/Isabella.jpg" },
+      { name: "Bella", nationality: "Thai", height: "163 cm", weight: "57kg", age: 26, cup: "C", description: "Fun playful Thai team member", image: "images/bella.jpg", hoverImages:["images/bella1.jpg","images/bella2.jpg","images/bella3.jpg"] },
+      { name: "Aya", nationality: "Chinese", height: "169 cm", weight: "55kg", age: 24, cup: "C", description: "Fun playful Chinese team member", image: "images/aya.jpg", hoverImages:["images/aya1.jpg","images/bella2.jpg","images/bella3.jpg"] },
       { name: "Melody", nationality: "Brazilian", height: "165 cm", weight: "50 kg", age: 22, cup: "D", description: "Gorgeous Brazilian team member", image: "images/Melody.jpg" }
     ],
     saturday: [
       { name: "Jade", nationality: "Chinese", height: "165 cm", weight: "50 kg", age: 20, cup: "C", description: "Sweet and fresh student style", image: "images/Jade.jpg" , hoverImages:["images/jade1.jpg","images/jade2.jpg","images/jade3.jpg"]},
       { name: "Mickey", nationality: "Chinese", height: "168 cm", weight: "47kg", age: 21, cup: "B", description: "Young pretty Chinese team member", image: "images/mickey.jpg", hoverImages:["images/mickey1.jpg","images/mickey2.jpg","images/mickey3.jpg"] },
+      { name: "Lily", nationality: "Chinese", height: "168 cm", weight: "47kg", age: 21, cup: "B", description: "Fun playful Chinese team member", image: "images/lily.jpg", hoverImages:["images/lily1.jpg","images/lily2.jpg","images/lily3.jpg"] },
       { name: "Wendy", nationality: "Chinese", height: "170 cm", weight: "49 kg", age: 21, cup: "B", description: "Tall, stunning Chinese beauty", image: "images/Wendy.jpg", hoverImages:["images/wendy1.jpg","images/wendyy2.jpg"] },
       { name: "Lucy", nationality: "Chinese", height: "162 cm", weight: "54 kg", age: 23, cup: "D", description: "Excellent Chinese massage practitioner", image: "images/lucy.jpg", hoverImages:["images/lucy1.jpg","images/lucy2.jpg","images/lucy3.jpg"] },
       { name: "Melody", nationality: "Brazilian", height: "165 cm", weight: "50 kg", age: 22, cup: "D", description: "Gorgeous Brazilian team member", image: "images/Melody.jpg" }
